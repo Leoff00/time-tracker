fac4531e5edfbf9f4f60882b1a945eaf8eb8c38c
leoff00 <leof0180@gmail.com>
2023-11-29 16:39:07 -0300

    chore: including current deps and feats

--- src/web/server.ts
+++ src/web/server.ts
@@ -1,13 +1,13 @@
-const express = require('express');
-const path = require('path');
-import * as moment from 'moment';
-import * as enums from '../enums';
+import * as moment from "moment";
+import * as enums from "../enums";
+import express from "express";
+import path from "path";
 const app = express();
-app.set('views', path.resolve(__dirname, '../../web'));
 
-app.set('view engine', 'ejs');
+app.set("views", path.resolve(__dirname, "../../web"));
+app.set("view engine", "ejs");
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.locals = {
     enums,
     moment,
